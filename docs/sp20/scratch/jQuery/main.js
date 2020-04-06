1708d1408f85fb9745672282f492c0670746e064
Doug Rosman <drosman@saic.edu>
2020-04-06 15:34:50 -0500

    cool

--- docs/sp20/scratch/jQuery/main.js
+++ docs/sp20/scratch/jQuery/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
-    $(".center").addClass("cool");    
+    $(".center").addClass("cool");
 
+    for(let i = 0; i < 10; i++){
+        let currBox = "box"+i;
+        $("body").append("<div>"+currBox+"</div>");
 
+        
+        $("div").addClass(currBox);
+
+        console.log(currBox);
+        $("."+currBox).css({
+            "background-color": `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`,
+            width: `${100+Math.random()*100}`,
+            height: "100px",
+            margin: "20px"
+        });        
+        // console.log($("body"));
+    }
 });
