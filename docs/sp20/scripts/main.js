02e3db48123d638639bc23fe9b5f8eafde1255c8
Doug Rosman <drosman@saic.edu>
2020-04-08 00:29:33 -0500

    add dropdown menu

---
+++ docs/sp20/scripts/main.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+
+
+
+// $(".drop-down").hover(function(){
+    
+//     $(".drop-down-menu").toggle("fast");
+
+// });
+
+// $(".drop-down-menu").hover(function(){
+    
+//     $(".drop-down-menu").toggle("fast");
+
+// });
+
+
+
+// // video to-do: toggle in vanilla JS vs jquery
+
+// });
