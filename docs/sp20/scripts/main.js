aec682499afbced8bc6b7984592a5a74216e8423
Doug Rosman <drosman@saic.edu>
2020-04-08 01:11:32 -0500

    add dropdown menu

--- docs/sp20/scripts/main.js
+++ docs/sp20/scripts/main.js
@@ -1,21 +1,17 @@
-// $(document).ready(function(){
+$(document).ready(function(){
+
+    // automatically adds this menu to a dropdown-menu class
+    $(".dropdown-menu").html(`<a class="dropdown-item" href="/sp20/assignments/01-internet-review/">01: Internet Review</a>
+    <a class="dropdown-item" href="/sp20/assignments/02a-html-basics">02a: HTML Basics</a>
+    <a class="dropdown-item" href="/sp20/assignments/02b-lialina-reading">02b: Lialina Reading</a>
+    <a class="dropdown-item" href="/sp20/assignments/03-portfolio-1">03: Portfolio I</a>
+    <a class="dropdown-item" href="/sp20/assignments/04-portfolio-2">04: Portfolio II</a>
+    <a class="dropdown-item" href="/sp20/assignments/05-advanced-css/">05: Advanced CSS</a>
+    <a class="dropdown-item" href="/sp20/assignments/06-intro-javascript/">06: JavaScript Intro</a>
+<a class="dropdown-item" href="/sp20/assignments/07-jquery/">07: jQuery + media</a>`);
 
 
 
-// $(".drop-down").hover(function(){
-    
-//     $(".drop-down-menu").toggle("fast");
+// video to-do: toggle in vanilla JS vs jquery
 
-// });
-
-// $(".drop-down-menu").hover(function(){
-    
-//     $(".drop-down-menu").toggle("fast");
-
-// });
-
-
-
-// // video to-do: toggle in vanilla JS vs jquery
-
-// });
+});
