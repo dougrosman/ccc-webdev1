1534ec6468c390990445109234207c76141dbc2d
Doug Rosman <drosman@saic.edu>
2020-04-08 12:06:09 -0500

    create dynamic menu in javascript

--- docs/sp20/scripts/menu.js
+++ docs/sp20/scripts/menu.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
       </div>
        <a class="menu-item yellow-h" href="/sp20/resources">Resources</a>
       <a class="menu-item green-h" href="/sp20/lectures">Online Lectures</a>
-      <a class="menu-item blue-h" href="https://codepen.io/dougrosman/collections">Codepen</a>
+      <a class="menu-item blue-h" href="https://codepen.io/dougrosman/collections" target="_blank">Codepen</a>
       <a class="menu-item canvas-red-h" href="https://canvas.colum.edu/" target="_blank">Canvas</a>`);
    }
 
@@ -49,7 +49,7 @@ $(document).ready(function(){
       </div>
        <a class="menu-item yellow-h" href="/ccc-webdev1/sp20/resources">Resources</a>
       <a class="menu-item green-h" href="/ccc-webdev1/sp20/lectures">Online Lectures</a>
-      <a class="menu-item blue-h" href="https://codepen.io/dougrosman/collections">Codepen</a>
+      <a class="menu-item blue-h" href="https://codepen.io/dougrosman/collections" target="_blank">Codepen</a>
       <a class="menu-item canvas-red-h" href="https://canvas.colum.edu/" target="_blank">Canvas</a>`);
    }
 
