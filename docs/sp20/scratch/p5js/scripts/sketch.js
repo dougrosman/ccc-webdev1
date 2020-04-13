5e0e621b89865c8d28832eba08e7db122663c16b
Doog <63171864+doogwebdevgittutorial@users.noreply.github.com>
2020-04-13 13:47:35 -0500

    cool

---
+++ docs/sp20/scratch/p5js/scripts/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    // background('red');
+    
+    // rect(100, 200, 200, 200);
+    ellipse(mouseX, mouseY, mouseX, mouseX);
+    
+  }
